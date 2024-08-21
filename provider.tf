ca292df59a1aa69cc51fac838c2880d264d14651
Gavin Pickles <163313639+GavinPickles@users.noreply.github.com>
2024-08-21 12:04:25 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vsphere = {
       source  = "hashicorp/vsphere"
-      version = ">=2.0.0"
+      version = "~=2.0.0"
       # fixed to 2.0 major but latest minor release of 2.x.x
     }
   }
